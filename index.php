8a4b0d95b872d25f5a8598d19c6eb8814fce681b
Francuzz1222 <bartekgithub@gmail.com>
2026-04-23 09:16:33 +0200

    dodanie strony głównej

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="p1.html"><button>1</button></a>
+     <a href="p2.html"><button>2</button></a>
+      <a href="p3.html"><button>3</button></a>
+       <a href="p4.html"><button>4</button></a>
+</body>
+</html>
